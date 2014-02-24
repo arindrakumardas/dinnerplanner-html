66c76e65238b35d29db6d38f85ef10321588f521
Arindra Kumar Das <arindra.das@live.com>
2014-02-24 16:51:03 +0100

    Working on select dish view

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -3,15 +3,39 @@ var SelectDishView = function (container,model) {
 	
 	
 		var sessionlist = "";
-		    for (var x = 0; x < model.getAllDishes.length; x++) {
-		      sessionlist += '<li>' +  model.getAllDishes("starter")[x].name + 
+		    for (var x = 0; x < model.getAllDishes.length+1; x++) {
+		      sessionlist += '<div id=add' + model.getAllDishes("starter")[x].id + '>' 
 			  
-			  '<li><img src="'+'images/'+model.getAllDishes("starter")[x].image+'" alt ="'+model.getAllDishes("starter")[x].image+'"></li>';
+			   + model.getAllDishes("starter")[x].name 
+			  
+		//	  '<button type="button">SAS</button>' + 
+			  
+			  // '<a href="#" id="dishDescriptionView">click me</a>' 
+			  
+			  + '<img src="'+'images/'+ model.getAllDishes("starter")[x].image 
+			  + '" alt ="'+ model.getAllDishes("starter")[x].image + 
+			  '">' + '</div>';
+			  
 			   console.log(model.getAllDishes("starter")[x].name);
 			}
-		   sessionlist += '<br></li>';
+		 //  sessionlist += '<br></li>';
 	    document.getElementById("selectDishView").innerHTML=sessionlist;
 	
-	
+		var idvalue = "";
+		    for (var x = 0; x < model.getAllDishes.length+1; x++) {
+  	
+  				idvalue += '#add' + model.getAllDishes("starter")[x].id ;
+  				$(idvalue).click(function() {
+  					$('#selectDishView').hide();
+  					$('#dishDescriptionView').show();
+  					});
+  			}
+  				
+								// 
+				// $("#add2").click(function() {
+				// 	$('#selectDishView').hide();
+				// 	$('#dishDescriptionView').show();
+				// 	});
+					
 }
  
