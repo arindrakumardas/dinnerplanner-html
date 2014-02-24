f76936b29631f549d1a52dd8ef5293091219fc0a
maggud <magnus.gudmandsen@gmail.com>
2014-02-24 14:31:25 +0100

    Finished the functionality of the summaryView. Needs styling.

--- js/view/summaryView.js
+++ js/view/summaryView.js
@@ -5,11 +5,19 @@ var SummaryView = function (container,model) {
   	// and/or ones that responed to interaction)
 	this.starter = container.find("#starter");
 	this.main = container.find("#main");
-	this.dessert = container.find("dessert");
+	this.dessert = container.find("#dessert");
 
 	this.starterName = container.find("#starterName");
 	this.mainName = container.find("#mainName");
-	this.dessertName = container.find("dessertName");
+	this.dessertName = container.find("#dessertName");
+
+	this.starterCost = container.find("#starterCost");
+	this.mainCost = container.find("#mainCost");
+	this.dessertCost = container.find("#dessertCost");
+
+	this.totalCost = container.find("#totalCost");
+
+	this.printRecipe = container.find("#printRecipe");
 
 	// this.starterName.innerHTML = 'asdf';
 	//Creating the components dynamically. Here we create the following HTML content:
@@ -21,23 +29,28 @@ var SummaryView = function (container,model) {
 	//and add it to the the exampleView 
 	
 	//Set the inital values of the components
-
-	//this.menu = model.getFullMenu();
 	
 	for(var i = 0; i < model.getFullMenu().length; i++) {
 		var dish = model.getFullMenu()[i];
 		if(dish.type == 'starter') {
-			this.starter.src = 'images/toast.jpg';
+			this.starter.attr('src', 'images/' + dish.image);
 			this.starterName.html(dish.name);
+			this.starterCost.html(model.getDishCost(dish) + ' SEK');
 		}
-		if(dish.type == 'main') {
+		if(dish.type == 'main dish') {
+			this.main.attr('src', 'images/' + dish.image);
 			this.mainName.html(dish.name);
+			this.mainCost.html(model.getDishCost(dish) + ' SEK');
 		}
 		if(dish.type == 'dessert') {
+			this.dessert.attr('src', 'images/' + dish.image);
 			this.dessertName.html(dish.name);
+			this.dessertCost.html(model.getDishCost(dish) + ' SEK');
 		}
 	}
 
+	this.totalCost.html(model.getTotalMenuPrice());
+	// document.getElementById("starter").src="/images/toast.jpg";
 	// this.starterName.html('TESTING');
 
 	// this.starterName.html(model.getSelectedDish('starter'));
@@ -53,8 +66,25 @@ var SummaryView = function (container,model) {
 	
 	//This function gets called when there is a change at the model
 	this.update = function(arg){
-		// this.starterName.html(model.getSelectedDish('starter'));
-		// this.mainName.html(model.getSelectedDish('main').name);
-		// this.dessertName.html(model.getSelectedDish('dessert').name);
+		for(var i = 0; i < model.getFullMenu().length; i++) {
+			var dish = model.getFullMenu()[i];
+			if(dish.type == 'starter') {
+				this.starter.attr('src', 'images/' + dish.image);
+				this.starterName.html(dish.name);
+				this.starterCost.html(model.getDishCost(dish) + ' SEK');
+			}
+			if(dish.type == 'main dish') {
+				this.main.attr('src', 'images/' + dish.image);
+				this.mainName.html(dish.name);
+				this.mainCost.html(model.getDishCost(dish) + ' SEK');
+			}
+			if(dish.type == 'dessert') {
+				this.dessert.attr('src', 'images/' + dish.image);
+				this.dessertName.html(dish.name);
+				this.dessertCost.html(model.getDishCost(dish) + ' SEK');
+			}
+		}
+
+	this.totalCost.html(model.getTotalMenuPrice());
 	}
 }
--- js/view/summaryViewController.js
+++ js/view/summaryViewController.js
@@ -1,8 +1,9 @@
 //NumGuestViewController Object constructor
 var SummaryViewController = function(view, model ) {
 
-	// view.backButton.click(function(){
-	// 	$('#homeView').show();
-	// 	$('#numGuestsView').hide();
-	// });
+	view.printRecipe.click(function(){
+		$('#homeView').show();
+		$('#numGuestsView').hide();
+		$('#summaryView').hide();
+	});
 }
