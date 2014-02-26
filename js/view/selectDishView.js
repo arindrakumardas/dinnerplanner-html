6f05ebaaa830995025a2ff1ee9985177ba6998d4
irene <paspatoufi@gmail.com>
2014-02-26 11:20:21 +0100

    wrapped up the code a bit

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -63,49 +63,35 @@ var SelectDishView = function (container,model) {
 // }
 
  		var sessionlist = "";
-		    for (var x = 0; x < model.getAllDishes("starter").length; x++) {
-		      sessionlist += '<li style="display:inline-block;width:200px;height:200px;">' +'<div id=starter' + model.getAllDishes("starter")[x].id + '>' 
-			  + '<img style="width:150px;height:150px;" src="'+'images/'+ model.getAllDishes("starter")[x].image 
-			  + '" alt ="'+ model.getAllDishes("starter")[x].image + 
-			  '">' + '<br>'
-			  + model.getAllDishes("starter")[x].name 
-			  + '</div>';	
-			}
+		for (var x = 0; x < model.getAllDishes("starter").length; x++) {
+		   	var dish = model.getAllDishes('starter')[x];
+		    sessionlist += '<li style="display:inline-block;width:200px;height:200px;">' +'<div id=starter' + dish.id + '>' + '<img style="width:150px;height:150px;" src="'+'images/'+ dish.image + '" alt ="'+ dish.image + '">' + '<br>'+ dish.name + '</div>';
+		}
 
-		    for (var x = 0; x < model.getAllDishes("main dish").length; x++) {
-		     sessionlist += '<li style="display:inline-block;width:200px;height:200px;">' +'<div id=main' + model.getAllDishes("main dish")[x].id + '>' 
-			 
-			  + '<img style="width:150px;height:150px;" src="'+'images/'+ model.getAllDishes("main dish")[x].image 
-			  + '" alt ="'+ model.getAllDishes("main dish")[x].image + 
-			  '">' + '<br>'
-			  + model.getAllDishes("main dish")[x].name 
-			  + '</div>';
-			  sessionlist += '<br><br></li>';	
-			}
+		for (var x = 0; x < model.getAllDishes("main dish").length; x++) {
+		   	dish = model.getAllDishes('main dish')[x];
+		   	sessionlist += '<li style="display:inline-block;width:200px;height:200px;">' +'<div id=starter' + dish.id + '>' + '<img style="width:150px;height:150px;" src="'+'images/'+ dish.image + '" alt ="'+ dish.image + '">' + '<br>'+ dish.name + '</div>';	
+			sessionlist += '<br><br></li>';	
+		}
 			
-		    for (var x = 0; x < model.getAllDishes("dessert").length; x++) {
-		     sessionlist += '<li style="display:inline-block;width:200px;height:200px;">' +'<div id=dessert' + model.getAllDishes("dessert")[x].id + '>' 
-			  
-			  + '<img style="width:150px;height:150px;" src="'+'images/'+ model.getAllDishes("dessert")[x].image 
-			  + '" alt ="'+ model.getAllDishes("dessert")[x].image + 
-			  '">' + '<br>'
-			  + model.getAllDishes("dessert")[x].name 
-			  + '</div>';
-			  sessionlist += '<br><br></li>';	
-			}
-		
+		for (var x = 0; x < model.getAllDishes("dessert").length; x++) {
+		   	dish = model.getAllDishes('dessert')[x];
+			sessionlist += '<li style="display:inline-block;width:200px;height:200px;">' +'<div id=starter' + dish.id + '>' + '<img style="width:150px;height:150px;" src="'+'images/'+ dish.image + '" alt ="'+ dish.image + '">' + '<br>'+ dish.name + '</div>';	
+		  	sessionlist += '<br><br></li>';	
+		}
 		
 	    document.getElementById("selectDishView").innerHTML=sessionlist;
 		
-		    for (var x = 0; x < model.getAllDishes("starter").length; x++) {
-				var idvalue = "";
-  				idvalue += '#starter' + model.getAllDishes("starter")[x].id ;
-  				$(idvalue).click(function() {
-  					$('#selectDishView').hide();
-  					$('#dishDescriptionView').show();
-					model.lastClickedDishID = idvalue;
-  					});
-  			}
+	    for (var x = 0; x < model.getAllDishes("starter").length; x++) {
+			var idvalue = "";
+			dish = model.getAllDishes('starter')[x];
+  			idvalue += '#starter' + dish.id ;
+  			$(idvalue).click(function() {
+  				$('#selectDishView').hide();
+  				$('#dishDescriptionView').show();
+				model.lastClickedDishID = idvalue;
+  			});
+  		}
   				
 				
 			    // Search functionality in the sidebar
